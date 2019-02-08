547bc3b6492cb3d0c8a10894fd728dfd6124cfff
thoaif <thoaif.ahmed@gmail.com>
2019-02-08 21:00:11 +0300

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -20,11 +20,22 @@ module.exports = function (app) {
       var input = req.query.input;
       var initNum = convertHandler.getNum(input);
       var initUnit = convertHandler.getUnit(input);
+    
+      console.log('input', input, 'num', initNum, 'unit', initUnit);
       var returnNum = convertHandler.convert(initNum, initUnit);
       var returnUnit = convertHandler.getReturnUnit(initUnit);
       var toString = convertHandler.getString(initNum, initUnit, returnNum, returnUnit);
       
       //res.json
+      if (initNum === null && !initUnit) return res.json({error: 'invalid number and unit'})
+      else if (initNum === null ) return res.json({error: 'invalid number'})
+      else if (!initUnit) return res.json({error: 'invalid unit'})
+      else res.json({initNum: initNum, 
+                     initUnit: initUnit, 
+                     returnNum: returnNum, 
+                     returnUnit: returnUnit, 
+                     string: toString})   
+    
     });
     
 };
